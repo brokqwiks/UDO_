db65682f440ebb64473393c56dc14d1e967be735
brokqwiks <brokqwiks@mail.ru>
2024-09-30 20:33:12 +0300

    2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
+import { TonConnectUIProvider } from "@tonconnect/ui-react"
 import { VisitingPage } from "../pages/visitingPage/ui/visitingPage"
 
 function App() {
   return (
+    <TonConnectUIProvider manifestUrl="https://githubraw.com/brokqwiks/UDO_-Visit/main/manifest.json">
     <div>
         <VisitingPage />
     </div>
+    </TonConnectUIProvider>
+ 
   )
 }
 
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,6 +1,7 @@
 import styles from './header.module.scss'
 
 export const Header = () => {
+
     return(
         <header className={styles.header}>
             <div className={styles.container}>
