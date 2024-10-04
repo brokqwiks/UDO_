01ec8b9f1ce1d8344c8ad78b3850b60443801f88
Danila Korotin <korotin.dravestil@gmail.com>
2024-10-04 19:16:18 +0400

    complete header and roadMap adaptiv

--- src/pages/visitingPage/ui/visitingPage.tsx
+++ src/pages/visitingPage/ui/visitingPage.tsx
@@ -1,11 +1,11 @@
 import styles from "./visitingPage.module.scss";
-// import { Header } from "../../../widgets/header";
+import { Header } from "../../../widgets/header";
 import { MainStart } from "../../../widgets/mainStart";
 import { About } from "../../../widgets/about";
 import { Community } from "../../../widgets/community";
 import { TonBlock } from "../../../widgets/tonBlock/ui/tonBlock";
 import { Partners } from "../../../widgets/partners";
-// import { RoadMap } from "../../../widgets/roadMap";
+import { RoadMap } from "../../../widgets/roadMap";
 import { Footer } from "../../../widgets/footer";
 // import { Learn } from "../../../widgets/learn";
 
@@ -13,7 +13,7 @@ export const VisitingPage = () => {
   return (
     <>
       <div className={styles.visitingPageWhiteContainer}>
-        {/* <Header /> */}
+        <Header />
         <MainStart />
 
          <About />
@@ -26,13 +26,13 @@ export const VisitingPage = () => {
       <TonBlock />
       
       <div className={styles.visitingPageBlackContainer}>
-        {/* <span className={styles.dottedWhite}></span>
+        {/* <span className={styles.dottedWhite}></span> */}
         <div className={styles.visitingPageBlack}>
-          <Learn />
+          {/* <Learn /> */}
           <RoadMap />
         </div>
 
-        <span className={styles.dottedWhite}></span> */}
+        {/* <span className={styles.dottedWhite}></span> */}
        
         <div className={styles.visitingPageBlack}>
           <Partners />
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,6 +1,8 @@
 import { useMyTranslate } from "../../../app/translationText/useMyTranslate";
-import { Button } from "../../../shared/ui/button";
 import styles from "./header.module.scss";
+import { Button } from "../../../shared/ui/button";
+import icons_language from "/svg/icon_language.svg";
+import icons_bar from "/svg/icon_bars.svg";
 
 export const Header = () => {
   const { t, changeLanguages, i18n } = useMyTranslate();
@@ -11,29 +13,58 @@ export const Header = () => {
         <div className={styles.logo}>
           <h1>UDO</h1>
         </div>
-        <nav className={styles.navigation}>
-          <ul>
-            <li>
-              <a href="#">{t("headerCommunity")}</a>
-            </li>
-            <li>
-              <a href="#">{t("headerLern")}</a>
-            </li>
-            <li>
-              <a href="#">{t("headerRoadMap")}</a>
-            </li>
-            <li>
-              <a href="#">{t("headerSupport")}</a>
-            </li>
-            <li>
-              {i18n.language == "ru" ? (
-                <Button onClick={() => changeLanguages("en")}>Eng</Button>
-              ) : (
-                <Button onClick={() => changeLanguages("ru")}>Ru</Button>
-              )}
-            </li>
-          </ul>
-        </nav>
+        {window.screen.width > 480 ? (
+          <nav className={styles.navigation}>
+            <ul>
+              <li>
+                <a href="#">{t("headerCommunity")}</a>
+              </li>
+              <li>
+                <a href="#">{t("headerLern")}</a>
+              </li>
+              <li>
+                <a href="#">{t("headerRoadMap")}</a>
+              </li>
+              <li>
+                <a href="#">{t("headerSupport")}</a>
+              </li>
+              <li>
+                {i18n.language == "ru" ? (
+                  <Button onClick={() => changeLanguages("en")}>Eng</Button>
+                ) : (
+                  <Button onClick={() => changeLanguages("ru")}>Ru</Button>
+                )}
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <nav className={styles.navigationAdaptiv}>
+            <ul>
+              <li>
+                {i18n.language == "ru" ? (
+                  <div className={styles.buttonAdaptiveLang}>
+                    <Button onClick={() => changeLanguages("en")}>
+                      <img src={icons_language} alt="" />
+                    </Button>
+                    <h4>Eng</h4>
+                  </div>
+                ) : (
+                  <div className={styles.buttonAdaptiveLang}>
+                    <Button onClick={() => changeLanguages("ru")}>
+                      <img src={icons_language} alt="" />
+                    </Button>
+                    <h4>Ru</h4>
+                  </div>
+                )}
+              </li>
+              <li>
+                <Button>
+                  <img src={icons_bar} alt="" />
+                </Button>
+              </li>
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
